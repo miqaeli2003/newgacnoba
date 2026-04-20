9596c9d0a910dc7161472208498cd7e55b20f922
miqaeli2003 <esebuamiqael@gmail.com>
2026-04-20 10:06:26 +0400

    Update script.js

--- script.js
+++ script.js
@@ -587,7 +587,7 @@ socket.on("message", (msg) => {
 
 socket.on("partnerSeen", ({ messageId }) => {
   const el = document.getElementById(`seen_${messageId}`);
-  if (el) { el.textContent = "✓✓"; el.classList.add("seen"); }
+  if (el) { el.textContent = ""; el.classList.add("seen"); }
 });
 
 socket.on("reacted", ({ messageId, emoji }) => {
