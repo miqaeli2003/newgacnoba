5f05530e7f8cc59a0f14a9b1505b3f7629c57b76
miqaeli2003 <esebuamiqael@gmail.com>
2026-06-29 07:33:13 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -593,7 +593,6 @@ function showNameError(msg) {
   nameError.style.display = "block";
   const _ov = document.getElementById("modalLoadingOverlay");
   if (_ov) _ov.style.display = "none";
-}
   nameInput.classList.add("error");
 }
 
